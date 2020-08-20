4bfe39455343fc6ac5004d897fe3e129fe2c8271
denysgashaw <denys.gashaw@gmail.com>
2020-08-20 23:27:54 +0300

    Created app for tracking user's actions

---
+++ src/actions/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Action(models.Model):
+    user = models.ForeignKey('auth.User', related_name='actions',
+                             db_index=True, on_delete=models.CASCADE)
+    verb = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ('created',)
